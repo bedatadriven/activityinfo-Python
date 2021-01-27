d328eba860a58f9d4a0fa57a4aba920893f38980
Maarten-Jan Kallen <4612238+mjkallen@users.noreply.github.com>
2021-01-27 14:53:08 +0100

    Fix generation of identifiers
    The counter in the GUID generator was reset at every call which means
    that the counter couldn't be used effectively for avoiding collisions on
    a single machine.

--- activityinfo/id.py
+++ activityinfo/id.py
@@ -1,6 +1,16 @@
 from cuid import CuidGenerator
 
+cuid = CuidGenerator()
+
 
 def generate_id():
-    cuid = CuidGenerator()
     return cuid.cuid()
+
+
+def cuid_counter():
+    """Return value of counter in CUID generator
+
+    Used for testing purposes only. Note that a call to the 'counter' property of CuidGenerator will increase the
+    counter value by 1.
+    """
+    return cuid.counter
--- activityinfo/tests/test_id.py
+++ activityinfo/tests/test_id.py
@@ -7,3 +7,10 @@ class Test(TestCase):
     def test_generate_id(self):
         new_id = activityinfo.generate_id()
         self.assertTrue(len(new_id) > 5)
+
+    def test_cuid_counter(self):
+        """Test to ensure that the counter in the CUID generator is global and therefore incremented at each call to
+        cuid_counter() """
+        old_value = activityinfo.id.cuid_counter()
+        new_value = activityinfo.id.cuid_counter()
+        self.assertTrue(new_value >= old_value + 1)
